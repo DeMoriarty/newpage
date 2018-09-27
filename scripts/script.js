6551cd29afc4870474904bd7fabae32343586e03
DeMoriarty <sn591771872@vip.qq.com>
2018-09-27 20:04:02 +0300

    seconds commit

---
+++ scripts/script.js
@@ -0,0 +1,120 @@
+import subWindow from './subWindow.js';
+
+var conlog = console.log;
+
+// NOTE:Header
+var header = document.getElementsByClassName('header')[0]
+var headerBackground = document.createElement('img')
+header.style.overflow = "hidden"
+headerBackground.src = "img/1.jpg"
+//headerBackground.height = header.clientHeight;
+//headerBackground.width = header.clientWidth;
+header.appendChild(headerBackground)
+
+// NOTE:Content
+var content = document.getElementsByClassName('content')[0]
+var contentstyle = getComputedStyle(content)
+
+// NOTE:Sidebar
+var sizebar = document.getElementsByClassName("side")[0]
+sizebar.style.height = contentstyle.height
+// NOTE:Footer
+var footer = document.getElementsByClassName("footer")[0]
+
+footer.style.top = Number(contentstyle.height.replace('px', '')) + Number(contentstyle.top.replace('px', ''))
+
+// NOTE: Sticky nav bar
+var navbar = document.getElementsByClassName("topnav")[0]
+var originalOffset = navbar.offsetTop
+window.onscroll = () => {
+    //let navbarPos = navbar.getBoundingClientRect();
+    let offset = navbar.offsetTop
+    if (window.pageYOffset >= originalOffset) {
+        navbar.classList.add('sticky')
+    } else {
+        navbar.classList.remove('sticky')
+    }
+}
+
+var navbarItems = navbar.children
+
+for (let i = 0; i < navbarItems.length; i++) {
+    let current = navbarItems[i];
+    current.style.display = "inline-block"
+    current.onmouseenter = () => { current.style.backgroundColor = 'rgb(0,60,90)'; }
+    current.onmouseleave = () => {
+        current.style.backgroundColor = 'rgb(60,108,128)';
+        if (current.href == window.location.href) {
+            current.style.backgroundColor = "rgb(0,30,30)"
+        }
+    }
+    if (current.href == window.location.href) {
+        current.style.backgroundColor = "rgb(0,30,30)"
+    }
+}
+
+var icons = document.createElement("link")
+icons.rel = "stylesheet"
+icons.href = "https://fonts.googleapis.com/icon?family=Material+Icons"
+document.head.appendChild(icons)
+
+var searchtag = document.getElementById("search")
+searchtag.style.display = "inline-flex"
+searchtag.style.cursor = "pointer"
+searchtag.draggable = false
+
+var searchbox = document.createElement('div');
+searchbox.className = "rightnavtag"
+searchbox.style.position = "absolute"
+searchbox.style.right = "50px"
+//navbar.appendChild(searchbox)
+
+
+var searchform = document.createElement('form')
+searchform.action = "http://www.google.com/search"
+searchform.target = "_blank"
+searchform.method = "GET"
+searchbox.appendChild(searchform)
+
+var searchbar = document.createElement("input")
+searchbar.type = "search"
+searchbar.name = "q"
+searchbar.style.display = "inline-block"
+
+var searchbutton = document.createElement("input")
+searchbutton.type = "submit"
+searchbutton.name = "submit"
+searchbutton.style.display = "inline-block"
+
+searchform.appendChild(searchbar);
+searchform.appendChild(searchbutton);
+
+var searchicon = document.getElementById("search-icon")
+searchicon.className = "material-icons"
+searchicon.innerText = "search"
+var closeicon = document.createElement("i")
+closeicon.id = "close-icon"
+closeicon.className = "material-icons"
+closeicon.innerText = "close"
+closeicon.style.display = "block"
+closeicon.style.float = "right"
+
+searchtag.open = false;
+searchtag.onclick = () => {
+    if (searchtag.open == false) {
+        searchtag.removeChild(searchicon);
+        navbar.appendChild(searchbox)
+        searchtag.appendChild(closeicon);
+        searchtag.open = true;
+    } else {
+        navbar.removeChild(searchbox);
+        searchtag.removeChild(closeicon);
+        searchtag.appendChild(searchicon);
+        searchtag.open = false
+    }
+
+}
+
+var subw1 = new subWindow(150, 250, 200, 300)
+subw1.appendTo(document.body)
+
---
+++ scripts/subWindow.js
@@ -0,0 +1,208 @@
+export default class SubWindow {
+    constructor(x, y, width, height) {
+        let windowmask = document.createElement('div')
+
+        windowmask.style.position = 'fixed'
+        windowmask.style.top = 0
+        windowmask.style.left = 0;
+        windowmask.style.width = '100%'
+        windowmask.style.height = '100%'
+        windowmask.style.zIndex = '100';
+        windowmask.style.backgroundColor = 'rgba(128,128,128,0.5)'
+        this.windowmask = windowmask;
+        window.prevTarget = windowmask
+
+
+        let body = document.createElement('div');
+        body.style.position = "fixed";
+        body.className = '-sh-subwindow-body'
+        body.style.top = x;
+        body.style.left = y;
+        body.style.height = height;
+        body.style.width = width;
+        body.style.minHeight = 150;
+        this.minHeight = 150;
+        body.style.minWidth = 250;
+        this.minWidth = 250;
+        this.initialX = 0;
+        this.initialY = 0;
+        this.initialHeight = 0;
+        this.initialWidth = 0;
+        windowmask.appendChild(body);
+        this.body = body
+
+        // NOTE:Borders
+        let topborder = document.createElement('div');
+        topborder.classList.add('-sh-topborder')
+        topborder.draggable = true;
+        topborder.ondragend = () => {
+            this.body.style.height = this.lastHeight;
+            this.body.style.top = this.lastTop;
+        }
+        topborder.ondragstart = (event) => {
+            this.initialY = this.body.offsetTop;
+            this.initialHeight = this.body.offsetHeight;
+            let clone = topborder.cloneNode(true);
+            clone.style.visibility = "hidden";
+            event.dataTransfer.setDragImage(clone, 0, 0)
+        }
+        topborder.ondrag = (event) => {
+            if (topborder.draggable === true) {
+                this.lastHeight = this.body.offsetHeight
+                this.lastTop = this.body.offsetTop
+                this.body.style.height = this.body.offsetHeight - event.offsetY
+                this.body.style.top = this.body.offsetTop + event.offsetY
+                if (this.body.offsetHeight <= Number(this.body.style.minHeight.replace('px', ''))) {
+                    this.body.style.height = Number(this.body.style.minHeight.replace('px', ''))
+                    this.body.style.top = this.initialY + this.initialHeight - this.minHeight;
+                }
+            }
+        }
+
+        //console.log(event.pageX, event.pageY, event.clientX, event.clientY)}
+        this.body.appendChild(topborder)
+
+        let bottomborder = document.createElement('div');
+        bottomborder.classList.add('-sh-bottomborder')
+        bottomborder.draggable = true;
+        bottomborder.ondragstart = (event) => {
+            let clone = bottomborder.cloneNode(true);
+            clone.style.visibility = "hidden";
+            event.dataTransfer.setDragImage(clone, 0, 0)
+        }
+        bottomborder.ondrag = (event) => {
+            if (bottomborder.draggable === true) {
+                body.style.height = body.offsetHeight + event.offsetY
+            }
+        }
+        this.body.appendChild(bottomborder)
+
+        let leftborder = document.createElement('div');
+        leftborder.classList.add('-sh-leftborder')
+        leftborder.classList.add('no_ghost')
+        leftborder.draggable = true;
+        leftborder.ondragend = () => {
+            this.body.style.width = this.lastWidth;
+            this.body.style.left = this.lastLeft;
+        }
+        leftborder.ondragstart = (event) => {
+            this.initialX = this.body.offsetLeft;
+            this.initialWidth = this.body.offsetWidth;
+            let clone = leftborder.cloneNode(true);
+            clone.style.visibility = "hidden";
+            event.dataTransfer.setDragImage(clone, 0, 0)
+            //return false
+        }
+        leftborder.ondrag = (event) => {
+            if (leftborder.draggable === true) {
+                this.lastLeft = this.body.offsetLeft;
+                this.lastWidth = this.body.offsetWidth;
+                this.body.style.width = this.body.offsetWidth - event.offsetX;
+                this.body.style.left = this.body.offsetLeft + event.offsetX;
+                if (this.body.offsetWidth <= Number(this.body.style.minWidth.replace('px', ''))) {
+                    this.body.style.width = Number(this.body.style.minWidth.replace('px', ''));
+                    this.body.style.left = this.initialX + this.initialWidth - this.minWidth;
+                }
+            }
+        }
+        this.body.appendChild(leftborder)
+
+        let rightborder = document.createElement('div');
+        rightborder.classList.add('-sh-rightborder')
+        rightborder.draggable = true;
+        rightborder.ondragstart = (event) => {
+            let clone = rightborder.cloneNode(true);
+            clone.style.visibility = "hidden";
+            event.dataTransfer.setDragImage(clone, 0, 0)
+        }
+        rightborder.ondrag = (event) => {
+            if (rightborder.draggable === true) {
+                body.style.width = body.offsetWidth + event.offsetX
+            }
+        }
+        this.body.appendChild(rightborder)
+
+        // NOTE: DragBar
+        let dragbar = document.createElement('div')
+        dragbar.classList.add('-sh-dragbar')
+        dragbar.draggable = true;
+        dragbar.initialX = 0;
+        dragbar.initialY = 0;
+        dragbar.last_top = 0;
+        dragbar.last_left = 0;
+
+        dragbar.ondragstart = (event) => {
+            let clone = this.dragbar.cloneNode(true);
+            clone.style.visibility = "hidden";
+            event.dataTransfer.setDragImage(clone, 0, 0)
+            this.initialX = event.offsetX
+            this.initialY = event.offsetY
+        }
+        dragbar.ondragend = () => {
+            this.body.style.top = this.lastTop;
+            this.body.style.left = this.lastLeft;
+        }
+        dragbar.ondrag = (event) => {
+            if (this.dragbar.draggable === true && event.offsetY > 0 && event.offsetX > 0) {
+                this.lastTop = this.body.offsetTop;
+                this.lastLeft = this.body.offsetLeft;
+                this.body.style.top = this.body.offsetTop + event.offsetY - this.initialY;
+                this.body.style.left = this.body.offsetLeft + event.offsetX - this.initialX;
+            }
+        }
+        window.ondragleave = (event) => {
+            if (event.offsetX < 0 || event.offsetY < 0) {
+                this.body.style.top = this.lastTop;
+                this.body.style.left = this.lastLeft;
+            }
+        }
+        this.body.appendChild(dragbar)
+        this.dragbar = dragbar
+
+        let title = document.createElement('span')
+        title.innerHTML = "New Window"
+        title.style.textAlign = "center"
+        title.style.verticalAlign = 'middle'
+        title.style.display = 'block'
+        title.style.position = 'relative'
+        window.addEventListener('load', () => {
+            this.title.style.top = (this.dragbar.offsetHeight - this.title.offsetHeight) / 2
+        })
+        this.dragbar.appendChild(title)
+        this.title = title
+
+        // NOTE: Content
+        let content = document.createElement('div')
+        content.style.width = 'calc(100% - 10px)';
+        content.style.left = 5;
+        content.style.position = 'absolute';
+        content.style.backgroundColor = "white"
+        window.addEventListener('load', () => {
+            this.content.style.height = 'calc(100% - ' + (this.dragbar.offsetHeight + 10).toString() + 'px)';
+            this.content.style.top = 5 + this.dragbar.offsetHeight;
+        })
+        this.body.appendChild(content)
+        this.content = content
+
+        // NOTE: Close Icon
+        let closeIcon = document.createElement('i')
+        closeIcon.classList.add("material-icons")
+        closeIcon.classList.add("unselectable")
+        closeIcon.innerText = "close"
+        closeIcon.style.position = "absolute"
+        closeIcon.style.padding = "5px"
+        closeIcon.style.right = "2.5px"
+        closeIcon.style.top = '2.5px'
+        closeIcon.style.backgroundColor = "grey"
+        closeIcon.style.float = 'right'
+        closeIcon.style.cursor = "pointer"
+        closeIcon.onclick = () => {
+            document.body.removeChild(windowmask)
+        }
+        body.appendChild(closeIcon)
+        this.closeIcon = closeIcon
+    }
+    appendTo(tag = document.body) {
+        tag.appendChild(this.windowmask)
+    }
+}
